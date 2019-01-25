b3201af42f1d00c083d7b3a07f8da5450b24f6d7
guozhi <33708876+gg17420@users.noreply.github.com>
2019-01-25 16:26:55 +0800

    Update browser.js

--- browser/browser.js
+++ browser/browser.js
@@ -12,6 +12,7 @@ export default class Browser {
 		this.agents = {};
 		this.browser = {};
 	}
+	// 新增浏览器配置
 	addTargetBrowser(config) {
 		if (config.name) {
 			this.targetBrowsers.push(config);
@@ -19,28 +20,32 @@ export default class Browser {
 			throw new Error('新增浏览器名称不可为空');
 		}	
 	}
+	// 获取代理版本号
 	getVersion(prefix, division) {
-		let regExp = new RegExp(prefix + division + '([\\d\.]+)', 'gi'),
-			result = regExp.exec(this.userAgent);
+		const regExp = new RegExp(`${prefix + division}([\\d\.]+)`, 'gi');
+		const result = regExp.exec(this.userAgent);
 		if (result) {
 			return result[1];
 		} else {
 			return null;
 		}		
 	}
+	// 常规代理版本号
 	useCommonRuler(prefix) {
-		return this.getVersion(prefix, '\/');
+		return this.getVersion(prefix, '/');
 	}
+	// IE代理版本号
 	useIERuler() {
 		return this.getVersion('msie', ' ') || '11.0';
 	}
+	// 设置代理
 	setAgent() {
-		for (let item of this.targetBrowsers) {
+		for (const item of this.targetBrowsers) {
 			if (item.name) {
 				item.prefix = item.prefix || item.name;
 				item.level = item.level || 1;
 				item.version = item.version || this.useCommonRuler;
-				if (this.userAgent.indexOf(item.prefix) !== -1) {
+				if (this.userAgent.incloudes(item.prefix)) {
 					this.agents[item.name] = item.version.bind(this)(item.prefix);
 				}
 			} else {
@@ -48,12 +53,14 @@ export default class Browser {
 			}
 		}
 	}
+	// 获取代理
 	getAgent() {
 		if (Object.keys(this.agents)) {
 			this.setAgent();
 		}
 		return this.agents;
 	}
+	// 验证代理
 	testAgent(config) {
 		if (Object.keys(this.agents)) {
 			this.setAgent();
@@ -65,12 +72,13 @@ export default class Browser {
 		}
 		return false;
 	}
+	// 获取具体浏览器
 	getBrowser() {
 		let level = Object.keys(this.agents).length;
 		if (level) {
 			this.setAgent();
 		}
-		for (let item of this.targetBrowsers) {
+		for (const item of this.targetBrowsers) {
 			if (this.agents[item.name] && item.level === level ) {
 				this.browser[item.name] = this.agents[item.name];
 				return {name: item.name, version: this.agents[item.name]};
@@ -78,11 +86,15 @@ export default class Browser {
 		}
 		return null;
 	}
+	// 验证具体浏览器
 	testBrowser(config) {
+		let name = '';
 		if (Object.keys(this.browser)) {
 			this.getBrowser();
+		} else {
+			name = config[this.browser.name];
 		}
-		if (config[browser.name] && parseFloat(browser.version) >= parseFloat(config[browser.name])) {
+		if (name && parseFloat(browser.version) >= parseFloat(config[browser.name])) {
 			return true;
 		}
 		return false;
