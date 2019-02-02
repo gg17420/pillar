f4c6d8e9452309e47a85bd7cbe2736ca42ef4fd3
guozhi <33708876+gg17420@users.noreply.github.com>
2019-02-02 09:36:26 +0800

    Update md5.js

--- md5/md5.js
+++ md5/md5.js
@@ -1,2 +1,24 @@
-export default {
+import utf from './utf8'
+
+let hexcase = 0,
+    b64pad = '',
+    chrsz = 8;
+
+function md5(input) {
+  let input = utf8(typeof input === 'string'? input: input.toString());
+  return binl2hex(core_md5(str2binl(input), input.length * chrsz));
 }
+
+function core_md5(x, len) {
+}
+
+function md5_cmn() {}
+function md5_ff() {}
+function md5_gg() {}
+function md5_hh() {}
+function md5_ii() {}
+function safe_add() {}
+function bit_rol() {}
+function str2binl() {}
+function binl2hex() {}
+export default md5
